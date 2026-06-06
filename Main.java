fc854dc70e0746da92e1704e77f81d488b94d726
AbhinavMoosaramtota <158453429+AbhinavMoosaramtota@users.noreply.github.com>
2026-06-06 19:29:54 +0530

    Replace Hello World with addition functionality

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter First Number: ");
+        int a=sc.nextInt();
+        System.out.print("Enter Second Number: ");
+        int b=sc.nextInt();
+        int addition=a+b;
+        System.out.println("The Addition of Two Numbers is: "+addition);
+            
     }
 }
